feed8818bf02c296015e0b19cfb6973bfdbbe194
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2024-12-19 21:31:57 +0100

    feat: html partials and a base template

--- main.go
+++ main.go
@@ -2,24 +2,70 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
+
+type PageData struct {
+	Title string
+	Path  string
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, data *PageData) {
+	partialsDir := "ui/templates/partials/"
+
+	partials, err := filepath.Glob(partialsDir + "*.tmpl.html")
+	if err != nil {
+		log.Fatal("Error loading partials: ", err)
+	}
+
+	tmplFiles := append(partials, "ui/templates/pages/"+tmpl+".tmpl.html")
+
+	t, err := template.ParseFiles(tmplFiles...)
+	if err != nil {
+		log.Fatal("Error parsing templates: ", err)
+	}
+
+	err = t.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var routeMap = map[string]struct {
+	template string
+	title    string
+}{
+	"/":           {"about", "About"},
+	"/experience": {"experience", "Experience"},
+	"/blog":       {"blog", "Blog"},
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	route, exists := routeMap[r.URL.Path]
+	if !exists {
+		http.NotFound(w, r)
+		return
+	}
+
+	data := &PageData{
+		Title: route.title,
+		Path:  r.URL.Path,
+	}
+
+	renderTemplate(w, route.template, data)
+}
 
 func main() {
 	fs := http.FileServer(http.Dir("./ui/static"))
+
 	http.Handle("GET /static/", http.StripPrefix("/static", fs))
+	http.HandleFunc("GET /", ServeHTTP)
 
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("./ui/templates/home.html")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.Execute(w, nil)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server started on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
